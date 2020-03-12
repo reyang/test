c76f7e0d4f1f308008e3afb502e37343f51df252
Reiley Yang <reyang@microsoft.com>
2020-03-12 13:28:27 -0700

    reterr

--- scripts/validate.py
+++ scripts/validate.py
@@ -3,15 +3,20 @@
 import glob
 import sys
 
+retval = 0
+
 for filename in glob.glob('**/*.md', recursive=True):
     with open(filename, 'rb') as file:
         content = file.read()
         if all(b < 128 for b in content):
             print('{} [PASS]'.format(filename))
         else:
+            retval += 1
             print('{} [FAIL]'.format(filename), file=sys.stderr)
             lineno = 0
             for line in content.splitlines():
                 if any(b > 127 for b in line):
                     print('  Ln:{} {}'.format(lineno, line), file=sys.stderr)
                 lineno += 1
+
+sys.exit(retval)
