be252258c8bbbc8847c4d9e41f4b37abd65b0cf8
Reiley Yang <reyang@microsoft.com>
2020-03-17 15:14:30 -0700

    add ILogger examples

---
+++ logs/DependencyInjectionILogger/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var services = new ServiceCollection()
+            .AddLogging(builder => {
+                builder.AddConsole();
+            })
+            .BuildServiceProvider();
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("Hello, world!");
+        (services as IDisposable)?.Dispose(); // this will flush the logs
+        return 0;
+    }
+}
---
+++ logs/HelloILogger/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogInformation("Hello, world!");
+        loggerFactory.Dispose(); // this will flush the logs
+        return 0;
+    }
+}
