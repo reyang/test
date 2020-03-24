be6e9d566c54a2df27aeda17449debdf44d86a3b
Reiley Yang <reyang@microsoft.com>
2020-03-23 23:49:16 -0700

    add OpenTelemetry trace demo

---
+++ traces/OpenTelemetryDemo/DemoExporter.cs
@@ -0,0 +1,22 @@
+﻿using OpenTelemetry.Trace.Export;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class DemoExporter : SpanExporter
+{
+    public override Task<ExportResult> ExportAsync(IEnumerable<SpanData> data, CancellationToken cancellationToken)
+    {
+        foreach (var spandata in data)
+        {
+            Console.WriteLine($"{spandata.Name}");
+        }
+        return Task.FromResult(ExportResult.Success);
+    }
+
+    public override Task ShutdownAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
