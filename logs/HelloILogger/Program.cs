2d9eb76103ae4b85341b01f432dfa9d5017bb418
Reiley Yang <reyang@microsoft.com>
2020-06-06 14:05:45 -0700

    try ilogger

--- logs/HelloILogger/Program.cs
+++ logs/HelloILogger/Program.cs
@@ -1,21 +1,41 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 class Program
 {
     static int Main(string[] args)
     {
+        Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+        var source = new ActivitySource("Demo");
+        ActivitySource.AddActivityListener(new ActivityListener
+        {
+            ActivityStarted = null,
+            ActivityStopped = null,
+            ShouldListenTo = source => true,
+            GetRequestedDataUsingParentId = (ref ActivityCreationOptions<string> options) => ActivityDataRequest.AllData,
+            GetRequestedDataUsingContext = (ref ActivityCreationOptions<ActivityContext> options) => ActivityDataRequest.AllData,
+        });
+
         using var loggerFactory = LoggerFactory.Create(builder =>
         {
             builder.AddConsole(options => { options.IncludeScopes = true; });
+            builder.Configure(options => options.ActivityTrackingOptions =
+                ActivityTrackingOptions.TraceId |
+                ActivityTrackingOptions.SpanId |
+                ActivityTrackingOptions.ParentId
+            );
         });
         var logger = loggerFactory.CreateLogger<Program>();
 
         logger.LogInformation("Hello, world!");
 
-        // Logging using a manual scope
-        using (logger.BeginScope(new { A = 1 }))
+        using (var activity = source.StartActivity("Foo"))
         {
-            logger.LogInformation("This is a test");
+            // Logging using a manual scope
+            using (logger.BeginScope(new { A = 1 }))
+            {
+                logger.LogInformation("This is a test");
+            }
         }
 
         return 0;
--- traces/DotNet5Demo/Program.cs
+++ traces/DotNet5Demo/Program.cs
@@ -7,17 +7,26 @@ class Program
 {
     static int Main(string[] args)
     {
+        Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
         var source = new ActivitySource("DemoSource");
         source.RegisterProcessor(new DemoProcessor());
+        Activity activity;
 
         using (var foo = source.StartActivity("foo"))
         {
             using (var bar = source.StartActivity("bar", ActivityKind.Client))
             {
-                bar.DisplayName = "baz"; // bar?.SetDisplayName("baz");
+                // bar?.DisplayName = "baz";
+                bar?.AddTag("tag1", "value1");
                 bar?.SetCustomProperty("alias", "reyang");
+                System.Console.WriteLine($"{bar.TraceStateString}");
+                activity = bar;
+                System.Console.WriteLine($"{activity.TraceId}");
             }
         }
+
+        System.Console.WriteLine($"{activity.Context}");
 
         return 0;
     }
