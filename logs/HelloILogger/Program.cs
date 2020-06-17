b0cb7f3b53aea0c369c7be5c487eff40c37683c0
Reiley Yang <reyang@microsoft.com>
2020-06-16 23:38:10 -0700

    make the logging more interesting

--- logs/HelloILogger/Program.cs
+++ logs/HelloILogger/Program.cs
@@ -34,11 +34,14 @@ class Program
             // Logging using a manual scope
             using (logger.BeginScope(new { A = 1 }))
             {
-                logger.LogInformation("This is a test");
+                using (logger.BeginScope(new { B = 1 }))
+                {
+                    logger.LogInformation("This is a test");
+                }
+                logger.Eat(new Food { Name = "burger from chick-fil-a", Price = 5.99 });
             }
         }
 
-        logger.Eat(new Food { Name = "burger from chick-fil-a", Price = 5.99 });
         logger.Sleep();
 
         return 0;
