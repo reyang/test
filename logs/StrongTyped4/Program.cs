8888537c46c9babaeabbfb0353ded97ba153a671
Reiley Yang <reyang@microsoft.com>
2022-02-18 15:27:02 -0800

    complex business types

--- logs/StrongTyped4/Program.cs
+++ logs/StrongTyped4/Program.cs
@@ -1,19 +1,28 @@
 using Microsoft.Extensions.Logging;
 
-public static partial class FoodSupplyLogs
+/*
+In this scenario, strong-types are coming from business model which is not tight to logs.
+
+* It is common for developers to log something that involves complex business types.
+* The developer could have no control over these business types - e.g. these types could come from a 3rd party library.
+* A log entry might contain multiple business types.
+*/
+
+// this type is not owned by the developer (e.g. coming from a 3rd party library)
+public class Food
 {
-    [LoggerMessage(
-        EventId = 100,
-        Message = "A `{productType}` (#{productCode}) recall notice was published for `{brandName} {productDescription}` produced by `{companyName}` ({recallReasonDescription}).")]
-    public static partial void FoodRecallNotice(
-        this ILogger logger,
-        LogLevel logLevel,
-        string brandName,
-        string productDescription,
-        string productType,
-        int productCode,
-        string recallReasonDescription,
-        string companyName);
+    public string BrandName;
+    public string ProductDescription;
+    public string ProductType;
+    public string CompanyName;
+}
+
+// this type is owned by the developer
+public class FoodRecallLogRecord
+{
+    public Food Food;
+    public int ProductCode;
+    public string RecallReasonDescription;
 }
 
 class Program
@@ -27,13 +36,26 @@ class Program
 
         var logger = loggerFactory.CreateLogger<Program>();
 
-        logger.FoodRecallNotice(
+        logger.Log(
             logLevel: LogLevel.Critical,
-            brandName: "Contoso",
-            productDescription: "Salads",
-            productType: "Food & Beverages",
-            productCode: 123,
-            recallReasonDescription: "due to a possible health risk from Listeria monocytogenes",
-            companyName: "Contoso Fresh Vegetables, Inc.");
+            eventId: 100,
+            state: new FoodRecallLogRecord
+            {
+                Food = new Food {
+                    BrandName = "Contoso",
+                    ProductDescription = "Salads",
+                    ProductType = "Food & Beverages",
+                    CompanyName = "Contoso Fresh Vegetables, Inc.",
+                },
+                ProductCode = 123,
+                RecallReasonDescription = "due to a possible health risk from Listeria monocytogenes",
+            },
+            exception: null,
+            formatter: (state, ex) =>
+            {
+                var record = state as FoodRecallLogRecord;
+                return $"A `{record.Food.ProductType}` (#{record.ProductCode}) recall notice was published for `{record.Food.BrandName} {record.Food.ProductDescription}` produced by `{record.Food.CompanyName}` ({record.RecallReasonDescription}).";
+            }
+        );
     }
 }
